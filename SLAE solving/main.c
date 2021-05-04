0a4b017fc7b882c0fe1abd6a994ca1cee5b2c5fd
Anton Labutin <Anton.Labutin.1997@yandex.ru>
2021-05-05 00:01:49 +0300

    Initial Commit

---
+++ SLAE solving/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SLAE solving
+//
+//  Created by Лабутин Антон Александрович on 05.05.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
